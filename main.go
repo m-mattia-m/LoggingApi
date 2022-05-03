bfbcf052d271200729cea7e597c77987aeb6c664
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-05-03 21:30:41 +0200

    add Setup for netlify

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"bookspreadLogging/users"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
+
+	lambda.Start(handler)
 	// Define the router
 	r := gin.Default()
 
@@ -30,4 +36,14 @@ func main() {
 	r.POST("/request", users.BasicAuth, users.CreateRequst)
 
 	r.Run(":3000")
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
+	}, nil
 }
